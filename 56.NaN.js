524f713d17190b3f3cadd52e0ebd1a8d89f6414a
burlaka sergii <burlaka01@gmail.com>
2024-03-19 15:43:04 +0200

    learning baNaNa

--- 56.NaN.js
+++ 56.NaN.js
@@ -16,3 +16,9 @@ console.log(`Number.isNaN(2 / {})`, Number.isNaN(2 / {}));
 console.log(`Number.isNaN(1 + +[3,5,6,])`, Number.isNaN(1 + +[3, 5, 6]));
 
 console.log(`Number.isNaN(1  +[3,5,6,])`, Number.isNaN(1 + [3, 5, 6]));
+
+console.log(`+"b" `, +"b");
+console.log(`parseInt('b') `, parseInt('b'));
+console.log(`parseInt('1b') `, parseInt('1b'));
+
+console.log(`"b" + "a" + +"a" + "a" `, "b" + "a" + +"a" + "a");
